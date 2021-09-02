2dcffa636a4b9e20cfd48dd2bb0323046818838a
Ani Jekova <angelina.jekova@rubberduck.xyz>
2021-09-02 17:34:44 +0300

    Tab navigation

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,21 +1,56 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 import { HomeScreen } from '@src/pages/HomeScreen/HomeScreen';
 
-const Stack = createNativeStackNavigator();
+const HomeStack = createNativeStackNavigator();
+const SettingsStack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
+
+const HomeStackScreen = () => {
+  return (
+    <HomeStack.Navigator>
+      <HomeStack.Screen name="Home" component={HomeScreen} />
+    </HomeStack.Navigator>
+  );
+};
+
+const SettingsStackScreen = () => {
+  return (
+    <SettingsStack.Navigator>
+      <SettingsStack.Screen name="Account" component={HomeScreen} />
+    </SettingsStack.Navigator>
+  );
+};
 
 const Navigation = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen
-          name="Home"
-          component={HomeScreen}
-          options={{ title: 'Overview' }}
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            const iconName = route.name === 'Settings' ? 'ellipsis-v' : 'home';
+            const iconSize = focused ? size + 4 : size;
+
+            return <Icon name={iconName} size={iconSize} color={color} />;
+          },
+          tabBarActiveTintColor: 'black',
+          tabBarInactiveTintColor: 'gray',
+        })}>
+        <Tab.Screen
+          name="Start"
+          component={HomeStackScreen}
+          options={{ headerShown: false }}
         />
-      </Stack.Navigator>
+        <Tab.Screen
+          name="Settings"
+          component={SettingsStackScreen}
+          options={{ headerShown: false }}
+        />
+      </Tab.Navigator>
     </NavigationContainer>
   );
 };
